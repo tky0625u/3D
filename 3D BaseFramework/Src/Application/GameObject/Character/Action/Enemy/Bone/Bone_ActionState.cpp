aeeb99fa63df9c1192f94e151c7b34cf7992812c
tky0625u <kd1370302@st.kobedenshi.ac.jp>
2024-09-13 02:55:42 +0900

    enemy
    継承準備

---
+++ 3D BaseFramework/Src/Application/GameObject/Character/Action/Enemy/Bone/Bone_ActionState.cpp
@@ -0,0 +1,21 @@
+﻿#include "Bone_ActionState.h"
+
+void Bone_ActionState::Idol(std::shared_ptr<Enemy_ConText> context)
+{
+}
+
+void Bone_ActionState::Run(std::shared_ptr<Enemy_ConText> context)
+{
+}
+
+void Bone_ActionState::Attack(std::shared_ptr<Enemy_ConText> context)
+{
+}
+
+void Bone_ActionState::Hit(std::shared_ptr<Enemy_ConText> context, int _damage)
+{
+}
+
+void Bone_ActionState::Stumble(std::shared_ptr<Enemy_ConText> context)
+{
+}
---
+++ 3D BaseFramework/Src/Application/GameObject/Character/Action/Enemy/Bone/Bone_ActionState.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include"../Enemy_ActionState.h"
+
+class Bone_ActionState :public Enemy_ActionState
+{
+public:
+	Bone_ActionState()                                                         { Init(); }
+	~Bone_ActionState()                                               override {};
+
+	void Idol   (std::shared_ptr<Enemy_ConText> context)              override; //待ち
+	void Run    (std::shared_ptr<Enemy_ConText> context)              override; //走り
+	void Attack (std::shared_ptr<Enemy_ConText> context)              override; //攻撃
+	void Hit    (std::shared_ptr<Enemy_ConText> context, int _damage) override; //被弾
+	void Stumble(std::shared_ptr<Enemy_ConText> context)              override; //のけぞり
+
+protected:
+
+};
