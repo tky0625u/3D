2fc7b9891229dd2db1b7fcce3fa66d3f5e9a057d
tky0625u <kd1370302@st.kobedenshi.ac.jp>
2024-11-17 22:20:20 +0900

    GameOver
    値調整

--- 3D BaseFramework/Src/Application/GameObject/UI/Player/GameOver/GameOver.cpp
+++ 3D BaseFramework/Src/Application/GameObject/UI/Player/GameOver/GameOver.cpp
@@ -2,6 +2,8 @@
 
 void GameOver::Update()
 {
+	if (GetAsyncKeyState('Y') & 0x8000)m_alpha = 0.0f;
+
 	if (m_alpha < 1.0f)
 	{
 		m_alpha += m_alphaChange;
@@ -19,5 +21,6 @@ void GameOver::Init()
 	m_pTex = std::make_shared<KdTexture>();
 	m_pTex->Load("Asset/Textures/UI/Player/GameOver/Death.png");
 	m_color = { 1,1,1,m_alpha };
+	m_rect = { 0,0,1280,107 };
 
 }
